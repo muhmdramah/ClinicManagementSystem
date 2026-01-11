4acc1c977646d94506f18404280db849f019ba32
Mohammed Alabbasy <mohammedrammah83@gmail.com>
2026-01-11 04:24:48 +0200

    feat: Create authentication DTOs
    - Created 'RegisterDto' for user registration input.
    - Created 'LoginDto' for user authentication input.
    - Created 'AuthModel' to define the authentication response structure (Token, Expiry, etc.).

---
+++ ClinicManagementSystem.Api/Dtos/Authentication/AuthModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ClinicManagementSystem.Api.Dtos
+{
+    public class AuthModel
+    {
+        public string Message { get; set; }
+        public bool IsAuthenticated { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+        public DateTime ExpiresOn { get; set; }
+    }
+}
